34ed012425a4248716bef5bc1e22fa52f2924c76
Mex777 <fologeaalexandru@gmail.com>
2022-04-27 17:41:12 +0300

    added read checkboxes

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ const list = document.getElementById('list');
  * @param {*} name
  * @param {*} author
  * @param {*} size
- * @param {*} status
+ * @param {*} hasRead
  */
-function Book(name, author, size, status) {
+function Book(name, author, size, hasRead) {
   this.name = name;
   this.size = size;
-  this.status = status;
+  this.hasRead = hasRead;
   this.author = author;
+  this.toggleRead = () => {
+    this.hasRead = !this.hasRead;
+  };
 }
 
 /**
@@ -28,7 +31,7 @@ function addBook(book) {
  */
 function displayBook(book) {
   const card = document.createElement('div');
-  card.className = 'card';
+  card.className = 'card text-center';
 
   const body = document.createElement('div');
   body.className = 'card-body';
@@ -45,6 +48,18 @@ function displayBook(book) {
   size.textContent = book.size + ' pages';
   size.className = 'card-text';
 
+  const status = document.createElement('input');
+  status.type = 'checkbox';
+  status.role = 'switch';
+  status.className = 'form-check-input card-text';
+  status.checked = book.hasRead;
+  status.addEventListener('change', () => {
+    book.toggleRead();
+  });
+
+  const footer = document.createElement('div');
+  footer.className = 'card-footer text-end';
+
   const deleteButton = document.createElement('button');
   deleteButton.textContent = 'Delete';
   deleteButton.className = 'btn btn-danger';
@@ -56,15 +71,20 @@ function displayBook(book) {
   body.appendChild(name);
   body.appendChild(author);
   body.appendChild(size);
-  body.appendChild(deleteButton);
+  body.appendChild(status);
+  footer.appendChild(deleteButton);
   card.appendChild(body);
+  card.appendChild(footer);
   list.appendChild(card);
 }
 
 const form = document.getElementById('addBook');
 form.addEventListener('submit', (e) => {
-  currBook = new Book(bookName.value, author.value, pages.value, false);
+  currBook = new Book(
+      bookName.value, author.value, pages.value, readTheBook.checked,
+  );
   bookName.value = author.value = pages.value = '';
+  readTheBook.checked = false;
   addBook(currBook);
   e.preventDefault();
 });
